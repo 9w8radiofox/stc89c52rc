464cad59fb862910215984aff181278b17f821c7
Radio Fox <radiofoxinnovation@gmail.com>
2021-12-05 21:35:19 +0800

    created basic framework

--- 00 blinky/src/main.c
+++ 00 blinky/src/main.c
@@ -1,12 +1,3 @@
-#define ledPin P2  // To toggle P2 on the microcontroller
-
 int main(void){
-
-    while(1){}
-        ledPin = 0x00;  // P2 set to 0x00
-        for (int i=50; i>0; i--) {}
-        ledPin = 0xff;  // P2 set to 0xFF
-        for (int i=50; i>0; i--) {}
-    }
-    
+    return 0;
 }
